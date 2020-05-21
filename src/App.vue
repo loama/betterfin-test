4077d8b0504c2e751ebf43496ced7d57c1022f3d
Jaime López <loama18@gmail.com>
2020-05-21 16:27:28 -0500

    add login page and colors

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    abcef
+    <div id="navbar">
+      <img class="logo" src="./assets/logo.svg">
+    </div>
 
     {{accounts}}
     <transactions />
@@ -38,5 +40,39 @@ export default {
 </script>
 
 <style lang="sass">
+html, body
+  margin: 0
+  padding: 0
+  // COLORS
+  --primary: #F66652
+  --primary-interacted: rgba(240, 102, 82, 90)
+  --primary-lightened: rgba(240, 102, 82, 33)
+  --secondary: #537871
+  --secondary-interacted: rgba(83, 120, 113, 90)
+  --secondary-lightened: rgba(83, 120, 113, 33)
+  --stroke: #DFDAD8
+  --stroke-light: rgba(223, 218, 216, 90)
+  --stroke-darkened: rgba(83, 120, 113, 33)
+  --text-dark: #2C3340
+  --success: #A7C958
+  --white: #FFF
+  --almost-white: #FAF9F7
+
 #app
+  color: var(--text-dark)
+  padding-top: 56px
+
+  #navbar
+    background: #FFF
+    box-shadow: 1px 0 10px #596273
+    height: 48px
+    left: 0
+    position: fixed
+    top: 0
+    width: 100vw
+
+    .logo
+      display: block
+      margin: 12px auto
+      width: 104px
 </style>
--- src/views/loginPage.vue
+++ src/views/loginPage.vue
@@ -1,3 +1,92 @@
 <template>
-  <div></div>
+  <div id="loginPage">
+    <form id="loginForm">
+      <img class="logo" src="../assets/logo.svg">
+      <h2> Login to Betterfin </h2>
+
+      <div class="input">
+        <div class="placeholder"></div>
+        <input type="email">
+      </div>
+
+      <div class="input">
+        <div class="placeholder"></div>
+        <input type="password">
+      </div>
+
+      <input type="submit" value="Login">
+    </form>
+
+    <div id="loginImage"></div>
+  </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+#loginPage
+  background: #FFF
+  height: 100vh
+  left: 0
+  position: fixed
+  top: 0
+  width: 100vw
+  z-index: 100
+
+  #loginForm
+    text-align: center
+    width: 100vw
+
+    .logo
+      display: block
+      margin: 54px auto 36px auto
+      width: 104px
+
+    h2
+      margin-bottom: 48px
+
+    .input
+      position: relative
+
+      .placeholder
+        left: 0
+        position: absolute
+        top: 0
+
+      input
+        background: var(--almost-white)
+        border: 1px solid var(--stroke)
+        height: 48px
+        margin-bottom: 20px
+        outline: none
+        width: 304px
+
+        &:focus
+          border: 1px solid var(--stroke-darkened)
+
+    input[type="submit"]
+      background: var(--primary)
+      border: none
+      color: var(--white)
+      font-size: 16px
+      height: 44px
+      width: 304px
+
+@media (min-width: 1024px)
+  #loginPage
+    #loginForm
+      display: inline-block
+      height: 100vh
+      width: 50vw
+      vertical-align: top
+
+    #loginImage
+      background: #CBE2E1
+      height: 100vh
+      display: inline-block
+      width: 50vw
+</style>
