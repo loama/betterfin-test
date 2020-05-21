040a5bc9a6dd215f7ef5cbf26848a2727c96f788
Jaime López <loama18@gmail.com>
2020-05-21 17:57:26 -0500

    finish login page functionality

--- src/store/index.js
+++ src/store/index.js
@@ -8,9 +8,22 @@ export default new Vuex.Store({
     accounts: {},
     balances: [],
     trxs: {},
-    trxs_count: {}
+    trxs_count: {},
+    // UI
+    loginPage: {
+      style: {
+        display: 'block',
+        opacity: 1
+      }
+    }
   },
   mutations: {
+    login (state) {
+      state.loginPage.style.opacity = 0
+      setTimeout(() => {
+        state.loginPage.style.display = 'none'
+      }, 300)
+    },
     saveJsonData (state, payload) {
       state.accounts = payload.accounts
       state.balances = payload.balances
--- src/views/loginPage.vue
+++ src/views/loginPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="loginPage">
-    <form id="loginForm">
+  <div id="loginPage" v-bind:style="loginPageStyle">
+    <form id="loginForm" v-on:submit.prevent="show2FA()">
       <img class="logo" src="../assets/logo.svg">
       <h2> Login to Betterfin </h2>
 
@@ -15,15 +15,118 @@
       </div>
 
       <input type="submit" value="Login">
+
+      <div class="twoFactor" v-bind:style="{
+          display: twoFactor.styles.display,
+          opacity: twoFactor.styles.opacity
+        }">
+        <h2> Enter the code you received</h2>
+        <h4>We sent you a SMS with a code</h4>
+
+        <div class="code" v-on:click="focusCode()">
+          <input id="codeInput"
+                 type="number"
+                 v-model="twoFactor.code"
+                 v-on:keyup="checkCode()">
+
+          <div class="number"
+               v-bind:class="{
+                 active: twoFactor.code.split('').length === 0,
+                 filled: twoFactor.code.split('').length > 0
+               }">
+            {{twoFactor.code.split('')[0]}}
+          </div>
+
+          <div class="number"
+               v-bind:class="{
+                 active: twoFactor.code.split('').length === 1,
+                 filled: twoFactor.code.split('').length > 1
+               }">
+               {{twoFactor.code.split('')[1]}}
+          </div>
+
+          <div class="number"
+               v-bind:class="{
+                 active: twoFactor.code.split('').length === 2,
+                 filled: twoFactor.code.split('').length > 2
+               }">
+            {{twoFactor.code.split('')[2]}}
+          </div>
+
+          <div class="number"
+               v-bind:class="{
+                 active: twoFactor.code.split('').length === 3,
+                 filled: twoFactor.code.split('').length > 3
+               }">
+            {{twoFactor.code.split('')[3]}}
+          </div>
+
+          <div class="number"
+               v-bind:class="{
+                 active: twoFactor.code.split('').length === 4,
+                 filled: twoFactor.code.split('').length > 4
+               }">
+            {{twoFactor.code.split('')[4]}}
+          </div>
+
+          <div class="number"
+               v-bind:class="{
+                 active: twoFactor.code.split('').length === 5,
+                 filled: twoFactor.code.split('').length > 5
+               }">
+            {{twoFactor.code.split('')[5]}}
+          </div>
+        </div>
+      </div>
     </form>
 
-    <div id="loginImage"></div>
+    <div id="loginImage">
+      <h2>Small business deserves better.</h2>
+      <h4>Log in to evaluate your lending options and secure the financing that best fits your business needs.</h4>
+      <img src="../assets/login-image.png">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    loginPageStyle () {
+      return this.$store.state.loginPage.style
+    }
+  },
+  data () {
+    return {
+      twoFactor: {
+        code: '',
+        styles: {
+          display: 'none',
+          opacity: 0
+        }
+      }
+    }
+  },
+  methods: {
+    checkCode () {
+      if (this.twoFactor.code.split('').length > 5) {
+        this.login()
+      }
+    },
+    focusCode () {
+      const input = document.getElementById('codeInput')
+      input.focus()
+    },
+    login () {
+      this.$store.commit('login')
+    },
+    show2FA () {
+      this.twoFactor.styles.display = 'block'
+      setTimeout(() => {
+        this.twoFactor.styles.opacity = 1
+        this.focusCode()
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -34,10 +137,12 @@ export default {
   left: 0
   position: fixed
   top: 0
+  transition: all 0.3s
   width: 100vw
   z-index: 100
 
   #loginForm
+    position: relative
     text-align: center
     width: 100vw
 
@@ -63,7 +168,8 @@ export default {
         height: 48px
         margin-bottom: 20px
         outline: none
-        width: 304px
+        padding: 0 12px
+        width: 280px
 
         &:focus
           border: 1px solid var(--stroke-darkened)
@@ -72,9 +178,55 @@ export default {
       background: var(--primary)
       border: none
       color: var(--white)
+      cursor: pointer
       font-size: 16px
       height: 44px
-      width: 304px
+      outline: none
+      width: 308px
+
+    .twoFactor
+      background: var(--white)
+      height: 500px
+      left: 0
+      position: absolute
+      top: 0
+      transition: all 0.3s
+      width: 100%
+
+      .code
+        input
+          opacity: 0
+          pointer-events: none
+          position: absolute
+
+        .number
+          border: 1px solid var(--stroke)
+          border-radius: 2px
+          display: inline-block
+          font-size: 24px
+          height: 44px
+          line-height: 44px
+          margin: 8px
+          vertical-align: top
+          width: 40px
+
+          &.active
+            border: 1px solid var(--secondary)
+
+          &.filled
+            background: var(--secondary)
+            color: var(--white)
+
+  #loginImage
+    h2
+      margin: 120px 10% 24px 10%
+
+    h4
+      margin: 0 10% 32px 10%
+
+    img
+      margin-left: 10%
+      width: 80%
 
 @media (min-width: 1024px)
   #loginPage
