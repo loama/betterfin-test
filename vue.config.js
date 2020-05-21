391dee3d3720a734c7be98cfc2c75a31fafef9e3
Jaime López <loama18@gmail.com>
2020-05-21 12:41:04 -0500

    init vue capacitor project

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const exec = require('child_process').exec
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      {
+        apply: (compiler) => {
+          compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+            console.log('2')
+            // exec('node copyToElectron.js')
+            /* exec('', (err, stdout, stderr) => {
+              if (stdout) process.stdout.write(stdout);
+              if (stderr) process.stderr.write(stderr);
+            }); */
+          })
+        }
+      }
+    ]
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './' // production
+    : './'   // development
+}
